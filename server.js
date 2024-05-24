ec71cf46e7d2ade644fa26ecee4ab296e418eeed
seventhreality <104563775+seventh-reality@users.noreply.github.com>
2024-05-24 12:46:02 +0530

    Update server.js

--- server.js
+++ server.js
@@ -3,9 +3,8 @@ const path = require('path');
 const app = express();
 const port = 3000;
 
-// Serve static files
+// Serve static files from the 'public' directory
 app.use(express.static(path.join(__dirname, 'public')));
-app.use('/assets', express.static(path.join(__dirname, 'assets')));
 
 // Endpoint to serve scene configurations
 app.get('/scenes/:id', (req, res) => {
